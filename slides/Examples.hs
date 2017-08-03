60c93453f9c6aebd9949a02c6645c73e51751c7e
Peter Thiemann <peter.thiemann@gmail.com>
2017-08-03 13:06:54 +0200

    lunchtime

--- slides/Examples.hs
+++ slides/Examples.hs
@@ -22,3 +22,26 @@ prop_EuroUSD' x = euro (usd x) ~== x
 -- |almost equal operator
 x ~== y = abs (x - y) <= abs x * 1E-15
 
+
+-------------------------
+
+-- compute x to n-th power
+power :: Double -> Integer -> Double
+power x 0         = 1
+power x n | n > 0 = x * power x (n - 1)
+
+dumme_summe n = summe_acc n 0
+summe_acc 0 s = s
+summe_acc n s | n > 0 = summe_acc (n-1) (s+n)
+
+-- function over lists - examples
+summerize :: [String] -> String
+summerize []  = "None"
+summerize (x:[]) = "Only " ++ x
+summerize (x:y:[]) = "Two things: " ++ x ++ " and " ++ y
+summerize [_,_,_] = "Three things: ???"
+summerize _   = "Several things."   -- wild card pattern
+  
+-- not: nogo (x,x) = x
+nogo (x, y) | x == y = x
+nogo (x, y) = y
--- slides/M03examples.hs
+++ slides/M03examples.hs
@@ -1,21 +1,46 @@
 module M03examples where
 
+import Test.QuickCheck
+
 -- doubles [3,6,12] = [6,12,24]
 doubles :: [Integer] -> [Integer]
-doubles []     = undefined
-doubles (x:xs) = undefined
+doubles []     = []
+doubles (x:xs) = (2 * x) : doubles xs
+
+
+prop_doubles xs = doubles xs == doubles' xs
+
+prop_doubles' xs = doubles xs == map double xs
+
+prop_doubles'' xs = doubles xs == map (*2) xs
+
 
 -- map f [x1, x2, ..., xn] = [f x1, f x2, ..., fn]
-mymap f []     = undefined
-mymap f (x:xs) = undefined
+mymap :: (a -> b) -> ([a] -> [b])
+mymap f []     = []
+mymap f (x:xs) = f x : mymap f xs
+
+
+
 
 -- doubles with map
 doubles' xs = mymap double xs
 
 double :: Integer -> Integer
-double x = undefined
+double x = 2 * x
+
+
+
+
 
 -- filter odd [1,2,3,4,5] == [1,3,5]
 myfilter :: (a -> Bool) -> [a] -> [a]
-myfilter p []       = undefined
-myfilter p (x:xs) = undefined
+myfilter p []     = []
+myfilter p (x:xs)
+    | p x = x : rest
+    | motherwise = rest
+  where
+    rest = myfilter p xs
+    motherwise = True 
+
+myundefined = myundefined
